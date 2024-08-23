7560263ec56278db4109af56dbc9d82061b7681d
Vijayendra <38855805+vijayendrashri@users.noreply.github.com>
2024-08-23 06:34:00 +0000

    Update all basics of SpringBeans

--- tour-service/src/main/java/com/example/tourservice/TourManagementService.java
+++ tour-service/src/main/java/com/example/tourservice/TourManagementService.java
@@ -1,20 +1,23 @@
 package com.example.tourservice;
 
+import org.springframework.stereotype.Service;
+
 import com.example.tourservice.utilities.TourRepository;
 
+@Service
 public class TourManagementService {
     private TourRepository tourRepository;
 
-    
     public TourManagementService(TourRepository tourRepository) {
         this.tourRepository = tourRepository;
         createTour("Big Sur Retreat", 750, true);
-        createTour( "Day Spa Package", 200, false);
+        createTour("Day Spa Package", 200, false);
         createTour("Monterey to SantaBarbara Tour", 550, false);
         createTour("Kids L.A. Tour", 100, true);
         createTour("Islands of the Blue Dolphins Tour", 200, true);
         createTour("Endangered Specie Expedition", 250, true);
-    } 
+        createTour("Zoo Tour", 100, true);
+    }
 
     public Tour createTour(String title, Integer price, Boolean isKidFriendly) {
         return tourRepository.save(new Tour(title, price, isKidFriendly));
--- tour-service/src/main/java/com/example/tourservice/TravelAgentService.java
+++ tour-service/src/main/java/com/example/tourservice/TravelAgentService.java
@@ -1,13 +1,17 @@
 package com.example.tourservice;
 
+import org.springframework.stereotype.Service;
+
 import com.example.tourservice.utilities.TourRepository;
 
+@Service
 public class TravelAgentService {
-    private  TourRepository tourRepository;
+    private TourRepository tourRepository;
 
     public TravelAgentService(TourRepository tourRepository) {
         this.tourRepository = tourRepository;
     }
+
     public void displayTours() {
         tourRepository.findAll().stream().forEach(System.out::println);
     }
